ed06c3d99946eae96aa42182b7bae7e0228eb4b3
Reagan Morgan <123510936+reaganm-bot@users.noreply.github.com>
2023-04-04 00:53:09 -0500

    removed beginning line and adj1.

--- madlibs_beginner.py
+++ madlibs_beginner.py
@@ -1,13 +1,11 @@
 #start with paragraph that will have specific replacements; this will make it easier to determine placements and what types are needed for funny/good results. 
 print(" Give An Adjective: ") #prompts user to give an adjective.
 adj = input() #stores user input into an adj variable.
-print("Give Another Adjective: ")
-adj1 = input()
 print("Give A Person's Name: ")
 noun_person = input()
 print("Give A Verb: ")
 verb = input()
-print("Python is " + adj + "!" + " " + "Without python we would all be " + adj1 + " " + ":(" + "." + " Learning to code makes all of us happy.")
+print("Learning to code makes all of us " + adj)
 print("Ignore the impending AI takeover which will lead to " + noun_person + " being a slave at mcdonald's serving the AI overlords.") #allows for better readability of code.
 print(noun_person + " makes JAVASCRIPT frappes for the robots. He is not good at programming in Javascript so the robots " + verb + " him.")
 #issue during run @ 4/4/2023 @ 12:24AM : need to add a prompt so user knows when and what to type.
