f6cc36f079e8057a23979919277edb6d0cf9bf7f
Reagan Morgan <123510936+reaganm-bot@users.noreply.github.com>
2023-04-04 00:36:09 -0500

    created python file, ran and commented issues.

---
+++ madlibs_beginner.py
@@ -0,0 +1,5 @@
+#start with paragraph that will have specific replacements; this will make it easier to determine placements and what types are needed for funny/good results. 
+adj = input()
+
+print("Python is " + adj + "!")
+#issue during run @ 4/4/2023 @ 12:24AM : need to add a prompt so user knows when and what to type.
