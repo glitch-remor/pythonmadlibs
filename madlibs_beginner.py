a43a6dd661ed178a2a6cab4db6a9b1a8c5319075
Reagan Morgan <123510936+reaganm-bot@users.noreply.github.com>
2023-04-04 00:47:17 -0500

    added new line and noun user input.

--- madlibs_beginner.py
+++ madlibs_beginner.py
@@ -3,6 +3,10 @@ print(" Give An Adjective: ") #prompts user to give an adjective.
 adj = input() #stores user input into an adj variable.
 print("Give Another Adjective: ")
 adj1 = input()
-print("Python is " + adj + "!" + " " + "Without python we would all be " + adj1 + " " + ":(")
+print("Give A person's name: ")
+noun_person = input()
+print("Python is " + adj + "!" + " " + "Without python we would all be " + adj1 + " " + ":(" + "." + " Learning to code makes all of us happy.")
+print("Ignore the impending AI takeover which will lead to " + noun_person + " being a slave at mcdonald's serving the AI overlords.")
 #issue during run @ 4/4/2023 @ 12:24AM : need to add a prompt so user knows when and what to type.
-#issue during run @ 4/4/2023 @ 12:41 : used the wrong adj variable.
+#issue during run @ 4/4/2023 @ 12:41AM : used the wrong adj variable.
+#issue during run @ 4/4/2023 @ 12:46AM : forgot space before "Learning" string.
