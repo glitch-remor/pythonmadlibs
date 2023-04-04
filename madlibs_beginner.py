75965b7f05e3d6e22dc6e3f5775f370e9be3c510
Reagan Morgan <123510936+reaganm-bot@users.noreply.github.com>
2023-04-04 01:05:43 -0500

    finished?

--- madlibs_beginner.py
+++ madlibs_beginner.py
@@ -5,12 +5,15 @@ print("Give A Person's Name: ")
 noun_person = input()
 print("Give A Verb (present tense, negative action): ")
 verb = input()
-print("Give A Verb (prsent tense, positve action): ")
+print("Give A Verb (present tense, positve action): ")
 verb2 = input()
+print("Give An Adjective: ")
+adj2 = input()
 print("Learning to code makes all of us " + adj + ".")
 print("Ignore the impending AI takeover which will lead to " + noun_person + " being a slave at mcdonald's serving the AI overlords.") #allows for better readability of code.
 print(noun_person + " makes JAVASCRIPT frappes for the robots. He is not good at programming in Javascript so the robots " + verb + " him.")
 print(noun_person + " also makes PYTHON fries. He is very good at programming in python so the robots " + verb2 + " him.")
+print("Overall, " + noun_person + " makes the robots " + adj2 + ", so " + noun_person + " lives to see another day, maybe.")
 #issue during run @ 4/4/2023 @ 12:24AM : need to add a prompt so user knows when and what to type.
 #issue during run @ 4/4/2023 @ 12:41AM : used the wrong adj variable.
 #issue during run @ 4/4/2023 @ 12:46AM : forgot space before "Learning" string.
