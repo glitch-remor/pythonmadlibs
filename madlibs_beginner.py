22caf650ef1388d8300954d4c14373c024c57989
Reagan Morgan <123510936+reaganm-bot@users.noreply.github.com>
2023-04-04 00:41:49 -0500

    added another line and user input.

--- madlibs_beginner.py
+++ madlibs_beginner.py
@@ -1,5 +1,8 @@
 #start with paragraph that will have specific replacements; this will make it easier to determine placements and what types are needed for funny/good results. 
-adj = input()
-
-print("Python is " + adj + "!")
+print(" Give An Adjective: ") #prompts user to give an adjective.
+adj = input() #stores user input into an adj variable.
+print("Give Another Adjective: ")
+adj1 = input()
+print("Python is " + adj + "!" + " " + "Without python we would all be " + adj1 + " " + ":(")
 #issue during run @ 4/4/2023 @ 12:24AM : need to add a prompt so user knows when and what to type.
+#issue during run @ 4/4/2023 @ 12:41 : used the wrong adj variable.
